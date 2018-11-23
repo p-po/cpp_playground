efe78480949ad13c049a0bd7df2e173abf78903f
P.P <pponnuvel@gmail.com>
2018-11-23 13:10:44 +0000

    Use extended integer type

--- problem_sets/lists_strings/13.fib.cpp
+++ problem_sets/lists_strings/13.fib.cpp
@@ -1,11 +1,25 @@
+#include <cstring>
+#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+void get_string(unsigned __int128 t, char *s)
+{
+    size_t pos = 0;
+    while (t) {
+        s[pos++] = '0' + t % 10;
+        t = t / 10;
+    }
+}
+
 std::ostream& operator<<(std::ostream& os, unsigned __int128 t) {
-    string st("hi");
-    return os << str;
+    char s[128] = {0};
+    get_string(t, s);
+    reverse(s, s + strlen(s));
+    return os << s;
 }
 
 template<unsigned __int128 n> struct fib
